2019776e0d4a00e89d4bc6471a275840cd85f195
niksON <web.nikson@mail.ru>
2022-03-21 00:13:36 +0300

    initialize project
    Setting default configs and install necessary dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+      <div>Hello, world!</div>
+    </>
+  );
+}
+
+export default hot(App);
